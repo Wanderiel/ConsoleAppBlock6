5de95ca031c619d682d3ab5b8617d57f44f3d5eb
Wanderiel <dpavel82@yandex.ru>
2023-03-11 10:44:58 +0300

    Добавлено второе задание

---
+++ ConsoleAppB6P2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleAppB6P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player('☺', 10, 0);
+            Renderer renderer = new Renderer();
+
+            renderer.Draw(player.PositionX, player.PositionY, player.Symbol);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Renderer
+    {
+        public void Draw(int positionX, int positionY, char symbol = '@')
+        {
+            Console.SetCursorPosition(positionX, positionY);
+            Console.Write(symbol);
+        }
+    }
+
+    public class Player
+    {
+        public char Symbol { get; private set; }
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+
+        public Player(char symbol, int positionX, int positionY)
+        {
+            Symbol = symbol;
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+    }
+}
